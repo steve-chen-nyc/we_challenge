121d7340687edaa9d3f643666bebffc94f9e8554
steve-chen-nyc <steve.l.chen@gmail.com>
2017-03-27 19:47:17 -0400

    first commit

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(){
+    super()
+  }
+  render() {
+    return (
+        <h1> Hello World ! </h1>
+    )
+  }
+}
+
+export default App; 
