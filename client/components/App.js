5916119c13ddc9c720a9f280133a5960368a6501
steve-chen-nyc <steve.l.chen@gmail.com>
2017-03-27 20:15:55 -0400

    added react router

--- client/components/App.js
+++ client/components/App.js
@@ -6,9 +6,12 @@ class App extends Component {
   }
   render() {
     return (
+      <div>
         <h1> Hello World ! </h1>
+        {this.props.children}
+      </div>
     )
   }
 }
 
-export default App; 
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
+import Home from './components/Home.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  IndexRoute
+} from 'react-router-dom'
+
+ReactDOM.render((
+  <Router>
+    <App>
+      <Route exact path='/' component={Home}/>
+    </App>
+  </Router>
+  ),
+document.getElementById('root'))
